66c3963a34a4350b367f345510ddab6a2dbf8629
AVIgnatev <avignatev@at-consulting.ru>
2016-08-26 19:32:51 +0300

    streams

--- src/com/company/LogHandler.java
+++ src/com/company/LogHandler.java
@@ -4,9 +4,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.Duration;
+import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by AnVIgnatev on 26.08.2016.
@@ -26,7 +30,7 @@ public class LogHandler {
                     return record;
                 }
             }
-        } catch (IOException | LogRecordFormatException e) {
+        } catch (IOException /*| LogRecordFormatException */e) {
             e.printStackTrace();
         }
 
@@ -34,17 +38,41 @@ public class LogHandler {
     }
 
     public static void collectStatistics(Path logPath) throws InterruptedException {
-        //TODO think about hot file changes
+        try {
+
+            Map<String, Map<String, Record>> groupedByMethodAndId =
+//            System.out.println(
+                    Files.lines(logPath)
+                    .map(Parser::parse)
+                    .collect(Collectors.groupingBy(Record::getMethod, Collectors.groupingBy(Record::getId)));
+            Stream<Stream<Optional<Record>>> streamStream = groupedByMethodAndId.entrySet().stream().map(
+                    stringMapEntry -> stringMapEntry.getValue().entrySet().stream()
+                            .map((stringRecordEntry) -> stringMapEntry.getValue().values().stream()
+                                    .reduce(
+                                            ((record, record2) ->
+                                                    Duration.between(record.getTime(), record2.getTime()).toMillis()))));
+
+                   /* forEach(entry -> {
+                String method = entry.getKey();
+                Map<String, Record> recordMap = entry.getValue();
+                recordMap.
+            });
+*/
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+/*
         try (BufferedReader bufferedReader = Files.newBufferedReader(logPath)) {
-            long passed = 0;
+            long charsPassed = 0;
             String line;
             while ((line = bufferedReader.readLine()) != null) { //EOF
-                passed += line.length() + LINE_TERMINATION_CHARS_LEN;
+                charsPassed += line.length() + LINE_TERMINATION_CHARS_LEN;
                 Record enter = Parser.parse(line);
                 if (enter.getAction() == Action.ENTRY) {
-                    final long finalPassed = passed;
+                    final long finalCharsPassed = charsPassed;
                     threadPool.submit(() -> {
-                        Record exit = getExit(logPath, finalPassed, enter.getId());
+                        Record exit = getExit(logPath, finalCharsPassed, enter.getId());
                         StatisticsHandler.collect(enter, exit);
                     });
                 }
@@ -54,6 +82,6 @@ public class LogHandler {
         } finally {
             threadPool.shutdown();
             threadPool.awaitTermination(1, TimeUnit.HOURS);
-        }
+        }*/
     }
 }
--- src/com/company/Parser.java
+++ src/com/company/Parser.java
@@ -9,10 +9,10 @@ public class Parser {
     public static final int RECORDS_COUNT = 6;
     public static final String DATETIME_PATTERN = "yyyy-MM-dd'T'HH:mm:ss,SSS";
 
-    public static Record parse(String record) throws LogRecordFormatException {
+    public static Record parse(String record) /*throws LogRecordFormatException */{
         String[] split = record.split(SPACE);
-        if (split.length != RECORDS_COUNT)
-            throw new LogRecordFormatException("Wrong record format of " + record + " : wrong length");
+//        if (split.length != RECORDS_COUNT)
+//            throw new LogRecordFormatException("Wrong record format of " + record + " : wrong length");
         LocalDateTime time = LocalDateTime.parse(split[0], DateTimeFormatter.ofPattern(DATETIME_PATTERN));
         String clazz = split[2];
         Action action = Action.valueOf(split[3].toUpperCase());
