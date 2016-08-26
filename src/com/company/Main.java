f43468c4fd77731aa041c5d83d0d85a59cf878fd
AVIgnatev <avignatev@at-consulting.ru>
2016-08-26 16:52:52 +0300

    added timer

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,15 @@ import java.io.FileNotFoundException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 public class Main {
 
     public static final int PARAMS_COUNT = 1;
 
     public static void main(String[] args) throws FileNotFoundException {
+        LocalDateTime start = LocalDateTime.now();
         if (args.length != PARAMS_COUNT) {
             System.out.println("Please specify log file absolute path");
             System.exit(0);
@@ -20,6 +23,8 @@ public class Main {
             System.exit(0);
         }
         LogHandler.collectStatistics(logPath);
+
+        System.out.println("Work time: " + Duration.between(start, LocalDateTime.now()).toMillis());
         System.out.println(StatisticsHandler.getStatistics());
     }
 }
